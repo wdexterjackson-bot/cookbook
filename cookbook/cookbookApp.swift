350e03de485a1828cde1367713bb3a9d09a95b80
Dexter Jackson <WDexterJackson@gmail.com>
2026-08-05 09:47:59 -0400

    Initial Commit

---
+++ cookbook/cookbookApp.swift
@@ -0,0 +1,32 @@
+//
+//  cookbookApp.swift
+//  cookbook
+//
+//  Created by Dexter Jackson on 8/5/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct cookbookApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ cookbookTests/cookbookTests.swift
@@ -0,0 +1,18 @@
+//
+//  cookbookTests.swift
+//  cookbookTests
+//
+//  Created by Dexter Jackson on 8/5/26.
+//
+
+import Testing
+
+struct cookbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
